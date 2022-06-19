645d6f1ba36ef4c60ac51408f6c22c4b4528de7c
alsalmiw <alsalmiw@gmail.com>
2022-06-19 11:52:31 -0700

    clips-forms

---
+++ src/app/services/modal.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+
+interface IModal{
+  id: string;
+  visible: boolean;
+}
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ModalService {
+private modals:IModal[] = [];
+  constructor() { }
+
+  register(id:string){
+    this.modals.push({ 
+      id, 
+      visible: false
+    })
+    
+  }
+
+  unregister(id:string){
+    this.modals = this.modals.filter(modal => modal.id !== id)
+  }
+
+  isModalOpen(id:string): boolean {
+    return !!this.modals.find(element => element.id === id)?.visible
+  }
+
+  toggleModal(id:string): void {
+    //this.visible = !this.visible;
+    const modal = this.modals.find(element => element.id ===id)
+    if(modal){
+      modal.visible = !modal.visible
+    }
+  }
+}
---
+++ src/app/user/auth-modal/auth-modal.component.ts
@@ -0,0 +1,24 @@
+import { i18nMetaToJSDoc } from '@angular/compiler/src/render3/view/i18n/meta';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
+
+@Component({
+  selector: 'app-auth-modal',
+  templateUrl: './auth-modal.component.html',
+  styleUrls: ['./auth-modal.component.css']
+})
+export class AuthModalComponent implements OnInit, OnDestroy {
+
+  constructor(public modal:ModalService) { }
+
+  ngOnInit(): void {
+    this.modal.register('auth')
+    
+  }
+
+  ngOnDestroy(): void {
+    this.modal.unregister('auth')
+  }
+
+
+}
